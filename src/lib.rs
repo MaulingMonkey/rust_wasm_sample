0def95e70a7cc2732327326819965c3df139dac1
MaulingMonkey <git@maulingmonkey.com>
2019-06-17 16:14:45 -0700

    Add web-sys, duplicate html javascript logic in rust for comparison

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::*;
+use web_sys::*;
+
 mod common;
+
+#[wasm_bindgen(start)]
+pub fn browser_init () {
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    let content = document.get_element_by_id("content").unwrap();
+    let content : &HtmlElement = content.dyn_ref().unwrap();
+
+    content.set_inner_text(format!("hello_world_1: {}, hello_world_2: {}, hello_world_3: {}, hello_world_4: {}",
+        common::hello_world_1(),
+        common::hello_world_2(),
+        common::hello_world_3(),
+        common::hello_world_4()
+    ).as_str());
+}
