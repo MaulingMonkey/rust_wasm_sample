f5548ea6461e3a1791aa29d72e8e4c096e1f0204
MaulingMonkey <git@maulingmonkey.com>
2019-06-16 19:24:28 -0700

    Switch to wasm-pack, return a real string

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
+use wasm_bindgen::prelude::*;
+
 /// This can be called with `module.instance.exports.hello_world_1()`, without even using wasm-bindgen.
 /// However, no dead code stripping has occured, so it's comparatively huge - a 0.8MB raw `.wasm` file out of cargo.
 /// No wonder every WASM tutorial insists on using `wasm-pack`!
 /// To experiment, delete everything bellow `hello_world_3` and run:
-#[no_mangle]
+#[wasm_bindgen]
 pub fn hello_world_1() -> u32 {
     42
 }
 
 /// This also works... presumably the stdlib contains an allocator that just relies on e.g. a linear memory section,
 /// which requires no WASM imports.
-#[no_mangle]
+#[wasm_bindgen]
 pub fn hello_world_2() -> u32 {
     let mut v = vec!{1,2,3};
     v.push(4);
@@ -30,4 +32,10 @@ pub fn hello_world_2() -> u32 {
 // #[wasm_bindgen]
 pub fn hello_world_3() -> &'static str {
     "Hello, world!"
+}
+
+/// OK, now we need wasm-bindgen and `wasm-pack`.  And an `importObject`.  And the JS shims.
+#[wasm_bindgen]
+pub fn hello_world_4() -> String {
+    String::from("Hello, world!")
 }
